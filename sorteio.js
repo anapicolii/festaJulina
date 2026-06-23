a32f2fe9f5f65eab1f442d7531938586ac06b559
ana <aanapicolii@gmail.com>
2026-06-23 15:28:18 -0300

    first commit

---
+++ sorteio.js
@@ -0,0 +1,27 @@
+let p = document.querySelector(".numero");
+let btn = document.querySelector(".btn-sorteio");
+let btnModal = document.querySelector(".okModal");
+let modal = document.querySelector(".modal");
+
+btnModal.addEventListener("click", ()=>{
+    modal.close()
+})
+
+btn.addEventListener("click", ()=>{
+    p.textContent = "Número Sorteado:"
+
+    let valorMaximo = document.querySelector("#maximo").value
+
+    if(!valorMaximo.trim()){
+        modal.showModal();
+        return;
+    }
+    
+    let criado = document.createElement("span");
+    let sorteio = Math.floor(Math.random() * valorMaximo) + 1;
+
+    criado.textContent = ` ${sorteio}`;
+    criado.classList.add("estiloLegal");
+
+    p.append(criado);
+})
